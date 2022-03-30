6cb11bf4a8fa548673817345ebdc37b430c3a305
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-03-31 06:50:37 +0700

    edit error

--- models/User.js
+++ models/User.js
@@ -1,15 +1,45 @@
-const mongoose = require('mongoose')
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt')
+var userSchema = new Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+})
 
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err)
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err)
+                }
+                user.password = hash;
+                next()
+            })
+        })
+    }
+    else {
+        return next()
+    }
+})
 
-const UserSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    name: { type: String, require: true },
-    tel: { type: Number, require: true },
-    telsos:{type:Number, require: false},
-    bod: { type: Date, require: true },
-    address: {type:String, require:true},
-    location: {type:String,require:false},
-  })
-  
-  module.exports = mongoose.model('User', UserSchema)
+userSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if(err) {
+            return cb(err)
+        }
+        cb(null, isMatch)
+    })
+}
+
+module.exports = mongoose.model('User', userSchema)
