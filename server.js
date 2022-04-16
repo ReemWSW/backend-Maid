4ea5aa1b9aab3a85269fb7cf9aa23858f1b2d42e
ReemWSW <66340078+ReemWSW@users.noreply.github.com>
2022-04-16 10:00:11 +0700

    code H10

--- server.js
+++ server.js
@@ -25,9 +25,11 @@ app.use(routes)
 app.use(passport.initialize())
 require('./config/passport')(passport)
 
-const PORT = process.env.PORT || 3000
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
-)
+// app.listen(
+//   PORT,
+//   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+// )
